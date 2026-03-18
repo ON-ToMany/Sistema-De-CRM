3e7b866c9a7e8d352d65fe915dd4cafb45f6199c
jrs-neto <jrd.santosneto@gmail.com>
2026-03-18 20:28:00 -0300

    Deploy do Sistema de CRM

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,32 +3,25 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AuthModule } from './auth/auth.module';
-
-import { CategoriaEntity } from './categoria/entities/categoria.entity';
 import { CategoriaModule } from './categoria/categoria.module';
 import { ConfigModule } from '@nestjs/config';
 import { ClienteModule } from './Clientes/cliente.module';
-import { OportunidadeEntity } from './oportunidade/entities/oportunidade.entity';
 import { OportunidadeModule } from './oportunidade/oportunidade.module';
+import { ProdService } from './data/services/prod.service';
+import { DevService } from './data/services/dev.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_sistemacrm',
-      entities: [Usuario, Cliente, CategoriaEntity, OportunidadeEntity],
-      synchronize: true,
-    }),
+ConfigModule.forRoot(),
+TypeOrmModule.forRootAsync({
+	useClass: ProdService,
+    imports: [ConfigModule],
+}),
     UsuarioModule,
     AuthModule,
     ClienteModule,

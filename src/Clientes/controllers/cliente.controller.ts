ec39041c152d5a57bb882091faa66838ec509e31
alvarocsr2 <alvarocesarmm@gmail.com>
2026-03-18 13:57:39 -0300

    Criado a Classe de clientes,  pre definido  as relações bilateral, para futuros merges.

---
+++ src/Clientes/controllers/cliente.controller.ts
@@ -0,0 +1,56 @@
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, 
+    Param, Post, Put, ParseIntPipe } from "@nestjs/common";
+import { Cliente } from "../entities/cliente.entity";
+import { ClienteService } from "../services/cliente.service";
+
+@Controller("/clientes") 
+export class ClienteController {
+  
+  constructor(private readonly clienteService: ClienteService) { }
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Cliente[]> {
+    return this.clienteService.findAll();
+  }
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Cliente> {
+    return this.clienteService.findById(id);
+  }
+
+  @Get('/nome/:nome')
+  @HttpCode(HttpStatus.OK)
+  findBynome(@Param('nome') nome: string): Promise<Cliente[]> {
+    return this.clienteService.findByNome(nome);
+  }
+  @Get('/cpf/:cpf')
+  @HttpCode(HttpStatus.OK)
+  findByCpf(@Param('nome') cpf: string): Promise<Cliente[]> {
+    return this.clienteService.findByCpf(cpf);
+  }
+  @Get('/email/:email')
+  @HttpCode(HttpStatus.OK)
+  findByEmail(@Param('nome') email: string): Promise<Cliente[]> {
+    return this.clienteService.findByemail(email);
+  }
+
+  @Post('/cadastrar')
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() cliente: Cliente): Promise<Cliente> {
+    return this.clienteService.create(cliente);
+  }
+
+  @Put('/atualizar')
+  @HttpCode(HttpStatus.OK)
+  update(@Body() cliente: Cliente): Promise<Cliente> {
+    return this.clienteService.update(cliente);
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number){
+    return this.clienteService.delete(id);
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
+import { Cliente } from './Clientes/entities/cliente.entity';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { ClienteModule } from './Clientes/cliente.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
-})
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: "root",
+      password: "root",
+      database: "db_sistemacrm",
+      autoLoadEntities: true,
+      synchronize: true,
+   
+    }),
+  
+    ClienteModule,
+    
+    
+  ],
+}) 
 export class AppModule {}
